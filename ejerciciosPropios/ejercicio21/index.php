d7457db816bb80f4350f512616825f31a458fa89
gustavoPetruzzi <gustavopetruzzi19@gmail.com>
2017-04-07 17:56:54 -0300

    falta terminar el ejercicio 21

--- ejerciciosPropios/ejercicio21/index.php
+++ ejerciciosPropios/ejercicio21/index.php
@@ -1,3 +1,36 @@
 <?php
+    $archivo = fopen("misArchivos/palabras.txt", "r");
+    $stringPalabras = explode(" ", fread($archivo, filesize("misArchivos/palabras.txt")));
+    $palabras = array('uno' => 0, 'dos' => 0, 'tres' => 0, 'cuatro' => 0, 'mas' => 0);
+    foreach ($stringPalabras as $palabra) {
+        # code...
     
+        switch (strlen($palabra)) {
+            case 1:
+                $palabras['uno']++;
+                break;
+            case 2:
+                $palabras['dos']+= 1;
+                break;
+            case 3:
+                $palabras['tres']+= 1;
+                break;
+            case 4:
+                $palabras['cuatro']+= 1;
+                break;
+            default:
+                $palabras['mas']+= 1;
+                break;
+        }
+    }
+    echo "una palabra: ". $palabras['uno'];
+    echo "<br>";
+    echo "dos palabra: ". $palabras['dos'];
+    echo "<br>";
+    echo "tres palabra: ". $palabras['tres'];
+    echo "<br>";
+    echo "cuatro palabra: ". $palabras['cuatro'];
+    echo "<br>";
+    echo "mas palabra: ". $palabras['mas'];
+    echo "<br>";
 ?>
