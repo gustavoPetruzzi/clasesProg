448b554106823b61d31e2c733f9a3fd28685298d
Gustavo Petruzzi <gustavopetruzzi@gmail.com>
2017-04-10 04:25:22 -0300

    arreglo agregado de foto, falta testear

--- tp1/administracion.php
+++ tp1/administracion.php
@@ -6,16 +6,17 @@
 
         $tipoFoto = exif_imagetype($_FILES['foto']['tmp_name']);
         $tiposValidos = array(IMAGETYPE_JPEG, IMAGETYPE_BMP, IMAGETYPE_PNG, IMAGETYPE_GIF);
-        $fotoNueva = "fotos/".$_POST['dni']."-".$_POST['apellido'].".".pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
+        $fotoNueva = $_POST['dni']."-".$_POST['apellido'].".".pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
         $pathUsados = scandir("fotos");
+        
         
         if(in_array($tipoFoto, $tiposValidos) && $_FILES['foto']['size'] < 1024000 && !in_array($fotoNueva, $pathUsados))
         {
 
             $empleado = new empleado($_POST['nombre'], $_POST['apellido'], $_POST['dni'], $_POST['sexo'], $_POST['legajo'], $_POST['sueldo']);
             $archivo = fopen("empleados.txt","a");
-            move_uploaded_file($_FILES['foto']['tmp_name'], $fotoNueva);
-            $empleado->setPathFoto($fotoNueva);
+            move_uploaded_file($_FILES['foto']['tmp_name'], "fotos/".$fotoNueva);
+            $empleado->setPathFoto("fotos/".$fotoNueva);
             
             if(fwrite($archivo, $empleado->toString()."\r\n") === FALSE)
             {
@@ -26,6 +27,8 @@
                 echo "<a href='mostrar.php'>Mostrar Empleados</a>";
             }
         }
+        
+        
         
     }
     
