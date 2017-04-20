a1d0e86c0cff77b018a08f4c3f1c217008c793fe
Gustavo Petruzzi <gustavopetruzzi@gmail.com>
2017-04-20 00:30:43 -0300

    ALG

--- tp1/scripts/mostra.js
+++ tp1/scripts/mostra.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
     $.ajax({
-        url:'php/mostrar.php',
-        type:'GET',
-        dataType: 'json'
-    })
-    .done(function(data){
-        var info = data;
-        //var objeto = JSON.parse(info);
-        for (var element in info) {
-            var empleado = info[element];
-            alert(empleado.nombre);
-        }
-        
-    })
-    .fail(function(peticion, textStatus, errorThrown){
-        alert(peticion.status + " " + errorThrown)
-    });
+            url: 'php/mostrar.php',
+            type: 'GET',
+            dataType: 'json'
+        })
+        .done(function(data) {
+            var lineas = "";
+
+            for (var element in data) {
+                var empleado = data[element];
+                var src = "fotos/" + empleado.foto;
+                lineas = "<tr> <td>" + empleado.apellido + "</td>";
+                lineas += "<td>" + empleado.dni + "</td>";
+                lineas += "<td>" + "<img src =\"" + src + "\" width='150px' height='100px'/>";
+                lineas += "<td>" + "Aca van los botones" + "</td></tr>";
+                $("#empleados").find('tbody')
+                    .append(lineas);
+            }
+
+        })
+        .fail(function(peticion, textStatus, errorThrown) {
+            alert(peticion.status + " " + errorThrown)
+        });
 });
-
-
